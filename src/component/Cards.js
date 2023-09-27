4e5f10f5b976072351fe6fd0851ee1c997688121
akshayudupa7 <akshayudupa7@gmail.com>
2023-09-28 03:04:42 +0530

    new change

--- src/component/Cards.js
+++ src/component/Cards.js
@@ -1,22 +1,34 @@
-import * as React from "react";
-
 import Typography from "@mui/material/Typography";
 import { CardActionArea, Card, Grid } from "@mui/material";
 
-
-import "./Card.css"
-export default function ActionAreaCard({val}) {
+import "./Card.css";
+export default function ActionAreaCard({ val }) {
   return (
- 
-      <Grid item xs={12} sm={6} md={4} >
-        <Card sx={{backgroundSize:"100% 100%", backgroundImage: `linear-gradient(to bottom,rgba(0, 0, 0, 0.603), rgba(0, 0, 0, 0.639)), url(${val.images})`,'&:hover':{backgroundImage: `linear-gradient(to bottom,rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url(${val.images})`}}}>
-        <CardActionArea href="#"  className="cardactions">
-             <Typography variant="subtitle1" sx={{fontWeight:"bold"}}>/Live/</Typography>
-             <Typography  variant="h5" sx={{fontWeight:"bold",fontSize:"2rem"}}>{val.title}</Typography>
-             <Typography  variant="body1" sx={{fontWeight:"bold"}}>{val.sports}</Typography>
-          </CardActionArea>
-        </Card>
-      </Grid>
-  
+    <Grid item xs={12} sm={6} md={4}>
+      <Card
+        sx={{
+          backgroundSize: "100% 100%",
+          backgroundImage: `linear-gradient(to bottom,rgba(0, 0, 0, 0.603), rgba(0, 0, 0, 0.639)), url(${val.images})`,
+          "&:hover": {
+            backgroundImage: `linear-gradient(to bottom,rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url(${val.images})`,
+          },
+        }}
+      >
+        <CardActionArea href="#" className="cardactions">
+          <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
+            /Live/
+          </Typography>
+          <Typography
+            variant="h5"
+            sx={{ fontWeight: "bold", fontSize: "2rem" }}
+          >
+            {val.title}
+          </Typography>
+          <Typography variant="body1" sx={{ fontWeight: "bold" }}>
+            {val.sports}
+          </Typography>
+        </CardActionArea>
+      </Card>
+    </Grid>
   );
 }
